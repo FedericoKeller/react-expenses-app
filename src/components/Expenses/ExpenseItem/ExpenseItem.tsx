4ad06531f8549dab5baeef4feef6347d5194a8e2
Kelprim <43957146+Kelprim@users.noreply.github.com>
2022-01-08 11:56:11 -0300

    Working with State

--- src/components/Expenses/ExpenseItem/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem/ExpenseItem.tsx
@@ -1,5 +1,5 @@
 
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { ExpenseProps, Expense } from '../../../models/expenses.interface';
 import Card from '../../UI/Card/Card';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
@@ -8,13 +8,20 @@ import './ExpenseItem.css';
 const ExpenseItem: FC<ExpenseProps> = (props): JSX.Element => {
   const expenseItem: Expense = {...props.expenseItem};
   
+  const [title, setTitle] = useState(expenseItem.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+  }
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenseItem.date}/>
       <div className='expense-item__description'>
-        <h2>{expenseItem.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>{expenseItem.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
